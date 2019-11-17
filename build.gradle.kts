8992f41992893f9819ef2beb0551a8997657a86d
Nicholas Christopher <nwillc@gmail.com>
2019-11-17 14:20:21 -0500

    swagger-ui added

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
-	implementation("io.springfox:swagger-ui:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	implementation("org.bitbucket.tek-nik:spring-swagger-simplified:1.0.7")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
--- src/main/kotlin/com/example/demo/SwaggerConfig.kt
+++ src/main/kotlin/com/example/demo/SwaggerConfig.kt
@@ -1,20 +1,29 @@
 package com.example.demo
 
+import com.google.common.base.Predicates
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.builders.PathSelectors
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+
+
 
 @Configuration
 @EnableSwagger2
-class SwaggerConfig {
+class SwaggerConfig : WebMvcConfigurer {
     @Bean
     fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(PathSelectors.any())
+            .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
             .build()
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        //enabling swagger-ui part for visual documentation
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
 }
